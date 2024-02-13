12bedd0a591f2b476e6d37018aa9dda7699e3044
JadeJade888 <jade.marquez7750@alumnos.udg.mx>
2024-02-13 08:59:30 -0600

    parte 1

---
+++ app/src/main/java/com/example/a209755ihm/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.a209755ihm;
+
+import static java.lang.Thread.sleep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(3000);
+                }
+                catch(InterruptedException e){e.printStackTrace();}
+                finally {
+                    Intent intent = new Intent(MainActivity.this, inicio_sesion.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        thread.start();
+
+    }
+}
---
+++ app/src/main/java/com/example/a209755ihm/inicio_sesion.java
@@ -0,0 +1,12 @@
+package com.example.a209755ihm;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class inicio_sesion extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+    }
+}
