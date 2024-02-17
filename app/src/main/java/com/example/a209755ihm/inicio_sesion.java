64f2b7c26f8754b5e6a2750d7c655325b61cfed2
JadeJade888 <jade.marquez7750@alumnos.udg.mx>
2024-02-16 19:48:08 -0600

    parte 1, layout registro hecho,checar boton

--- app/src/main/java/com/example/a209755ihm/inicio_sesion.java
+++ app/src/main/java/com/example/a209755ihm/inicio_sesion.java
@@ -3,10 +3,19 @@ package com.example.a209755ihm;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class inicio_sesion extends AppCompatActivity{
+    private button registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+
+        registrar = (Button) findViewById(R.id.button2);
+        registrar.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(view v){
+        intent in
     }
 }
